4e41b743982d134d3dca9e9aba4d225b1af41815
Kirill Smirnov <34028713+smk-robotics@users.noreply.github.com>
2019-06-20 13:05:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int N = 4; // Размер матрицы
+
+vector<vector<int>> m(N, vector<int>(N));
+
+int main()
+{
+    int index = 1;
+
+    for (int lap = 0; lap <= round(N/2); lap++)
+    {
+        for (int i = lap; i < N - lap; i++)
+        {
+            m[lap][i] = index;
+            index++;
+        }
+
+        for (int i = lap + 1; i < N - lap; i++)
+        {
+            m[i][N - 1 - lap] = index;
+            index++;
+        }
+
+        for (int i = N - 2 - lap; i >= lap; i--)
+        {
+            m[N - 1 - lap][i] = index;
+            index++;
+        }
+
+        for (int i = N - 2 - lap; i > lap; i--)
+        {
+            m[i][lap] = index;
+            index++;
+        }
+    }
+
+    /// Вывод матрицы
+    cout << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%4d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
